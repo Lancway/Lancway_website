afa66da47fb9028b41cb696c99bc2ff0e6629acd
RitikRK96 <ritikkumar9525586511@gmail.com>
2025-02-13 11:35:39 +0530

    components.css added

--- Lancway/src/Components/AuthForm.jsx
+++ Lancway/src/Components/AuthForm.jsx
@@ -1,6 +1,7 @@
 // src/Components/AuthForm.jsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { signUp, login, logout } from "../firebase/Auth";
+import "./Components.css";
 
 const AuthForm = () => {
   const [email, setEmail] = useState("");
